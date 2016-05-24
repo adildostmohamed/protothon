9cb0fc3ad6f36af26dc86d39df9bf73d435eb8d9
Adil <adil_dostmohamed@yahoo.co.uk>
2016-05-24 23:33:47 +0100

    All initial file changes for passport integration for auth

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
-module.exports = function(app) {
+module.exports = function(app, passport) {
   //SET UP ROUTES
   //Home route
-  app.get('/', function(req, res) {
+  app.get('/', isLoggedIn, function(req, res) {
     res.render("home");
   });
 
@@ -14,5 +14,40 @@ module.exports = function(app) {
   app.get('/confirmation', function(req, res) {
     res.render("confirmation");
   });
+
+  //Login route
+  app.get('/login', function(req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render('login', { message: req.flash('loginMessage') });
+  });
+
+  //Handle the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/', // redirect to the homepage
+    failureRedirect : '/login', // redirect back to the login page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
+  //Signup form route
+  app.get('/signup', function(req, res) {
+   // render the page and pass in any flash data if it exists
+   res.render('signup', { message: req.flash('signupMessage') });
+  });
+
+  // Handle the signup form
+   app.post('/signup', passport.authenticate('local-signup', {
+       successRedirect : '/', // redirect to home
+       failureRedirect : '/signup', // redirect back to the signup page if there is an error
+       failureFlash : true // allow flash messages
+   }));
+
+  function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the login page
+    res.redirect('/login');
+  }
 
 };
